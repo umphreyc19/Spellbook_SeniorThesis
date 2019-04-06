9b3809209187003292f17caad7e3a09c4fab4a5e
Collin Beauchamp-Umphrey <umphreyc19@hanover.edu>
2019-04-06 16:11:05 -0400

    Added JSON file. Ref #8

--- app/src/main/java/com/example/zx/spellbook/Database.java
+++ app/src/main/java/com/example/zx/spellbook/Database.java
@@ -1,19 +1,13 @@
 package com.example.zx.spellbook;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import com.example.zx.spellbook.Card;
-import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
 
 public class Database extends AppCompatActivity{
+
 
 }
